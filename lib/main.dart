8c026c7ba3b1da04fed6a403e91398f60d1bd769
unknown <kevinjbaronp@gmail.com>
2023-08-26 18:13:48 -0500

    debug banner en falso

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,10 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+    debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
