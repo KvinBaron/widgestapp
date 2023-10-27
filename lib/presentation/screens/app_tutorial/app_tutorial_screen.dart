a265e517e59a5f5c5918bd1cac27103ef3a08b95
Kevin Baron <kevinjbaronp@gmail.com>
2023-10-27 11:12:34 -0500

    se agrego carpetas restantes

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart'; 
+
+class Apptutorialscreen extends StatelessWidget {
+
+static const name = "tutorial_screen";
+
+  const Apptutorialscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,5 +1,15 @@
 
 
+export 'package:widgestapp/presentation/screens/animated/animated_screen.dart';
+export 'package:widgestapp/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgestapp/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgestapp/presentation/screens/ui_controls/ui_controls_screen.dart';
+export 'package:widgestapp/presentation/screens/progress/progress_screen.dart';
+export 'package:widgestapp/presentation/screens/snackbar/smackbar_screen.dart';
 export  'package:widgestapp/presentation/screens/cards/cards_screen.dart';
 export  'package:widgestapp/presentation/screens/home/home_screen.dart';
 export  'package:widgestapp/presentation/screens/buttons/buttons_screen.dart';
+
+
+
+
