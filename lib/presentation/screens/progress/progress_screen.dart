810071c006218b5495f58541151be73a3a81f4c5
Kevin Baron <kevinjbaronp@gmail.com>
2023-10-31 11:42:26 -0500

    progress indicator terminado

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart'; 
 
 class ProgressScreen extends StatelessWidget {
@@ -32,7 +34,7 @@ class _ProgressView extends StatelessWidget {
           CircularProgressIndicator(strokeWidth: 2,backgroundColor: Colors.black45,),
            
           SizedBox( height: 10 ),
-          Text(" Progess Indicator Controlado"),
+          Text("Circular y lineal controlado"),
           SizedBox( height: 20 ),
 
           _ControlerProgressIndicator(),
@@ -46,6 +48,27 @@ class _ControlerProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300),(value){
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+               final progressValue = snapshot.data ?? 0;
+
+        return  Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                CircularProgressIndicator( value: progressValue ,strokeWidth: 2, backgroundColor: Colors.black12,),
+              const SizedBox(width : 20,),
+             Expanded(child: 
+             LinearProgressIndicator(value: progressValue)
+             )
+            ],
+          ),
+          );
+      }
+    );
   }
-}
+} 
